b3cdbd72e0c3c6065a6247af637228ffd8aae260
Jherey <jerry4christ2004@yahoo.com>
2017-12-24 02:53:52 +0100

    Add jumbotron to view page

--- view.php
+++ view.php
@@ -1,5 +1,6 @@
 <?php
 include_once("db.php");
+include_once("header.html");
 ?>
 
 <html>
@@ -8,26 +9,33 @@ include_once("db.php");
 </head>
 
 <body>
-	<a href="index.php">Go Home!</a>
+	<a href="index.php" style="margin-left: 10px;">Home</a>
 	<br/><br/>
 
+	<div class="container">
+		<div class="jumbotron">
+			<?php
+				//getting id from url
+				$id = $_GET['id'];
+
+				//selecting data associated with this particular id
+				$result = mysqli_query($mysqli, "SELECT * FROM list WHERE id=$id");
+
+				while($res = mysqli_fetch_array($result))
+				{
+					$title = $res['title'];
+					$date = $res['date'];
+					$description = $res['description'];
+				}
+
+				echo "$title"."<br >";
+				echo "$description";
+				echo "$date";
+			?>
+		</div>
+	</div>
+
+
 <?php
-//getting id from url
-$id = $_GET['id'];
-
-//selecting data associated with this particular id
-$result = mysqli_query($mysqli, "SELECT * FROM list WHERE id=$id");
-
-while($res = mysqli_fetch_array($result))
-{
-	$title = $res['title'];
-	$date = $res['date'];
-	$description = $res['description'];
-}
-
-echo "$title"."<br >";
-echo "$description";
+include_once("footer.html");
 ?>
-
-</body>
-</html>
